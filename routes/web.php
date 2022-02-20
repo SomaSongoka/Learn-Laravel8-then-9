d62d283e8f6d846d87717eaef1dc916185af2d2b
Josh L. Minga <joshlminga@gmail.com>
2022-02-20 16:46:56 +0300

    set-up basic pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts', function () {
+    return view('posts');
+});
+
+Route::get('post', function () {
+    return view('post');
+});
