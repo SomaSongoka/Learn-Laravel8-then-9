9dc8dac302915b288cf37c3f281e7e1d2c72a4f3
Josh L. Minga <joshlminga@gmail.com>
2022-02-22 19:39:59 +0300

    Blade Components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,34 @@
+{{-- Since we are using Balde components we should declaire the component slot--}}
 
-{{--Extend the Layout--}}
-@extends('layout')
+{{--We can pass variable to our components as attribute eg--}}
+<?php $description = 'This is a description'; ?>
+<x-app title="This value will be assigned to variable title" description="{{$description}}">
 
-{{--Declare the section --}}
-@section('content')
+    {{--   Or we can declaire our variable as slot eg: our variable will be full_name --}}
+    <x-slot name="full_name">
+        John Doe
+    </x-slot>
+
+{{-- Or we can just add values inside this area and it will be assign to variable slot inside our component --}}
     @foreach ($posts as $post)
-    <div class="col-md-4 col-sm-12 mb-5 {{ $loop->even ? 'foobar' : 'foo' }}">
-        <div class="card">
-            <div class="card-body">
-                <a href="/posts/{{$post->slug}}">
-                    <h1 class="card-title">{{ $post->title }} </h1>
-                </a>
-                {{ $post->excerpt }}
-                <p class="card-text">
-                    <small class="text-muted">Last updated {{ $post->date }}</small>
-                </p>
+        <div class="col-md-4 col-sm-12 mb-5 {{ $loop->even ? 'foobar' : 'foo' }}">
+            <div class="card">
+                <div class="card-body">
+                    <a href="/posts/{{$post->slug}}">
+                        <h1 class="card-title">{{ $post->title }} </h1>
+                    </a>
+                    {{ $post->excerpt }}
+                    <p class="card-text">
+                        <small class="text-muted">Last updated {{ $post->date }}</small>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
     @endforeach
-@endsection
+</x-app>
+
+
+
+
+
+
