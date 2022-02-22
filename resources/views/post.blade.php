b0855d133981df918daddf08266a9714b938348a
Josh L. Minga <joshlminga@gmail.com>
2022-02-22 19:16:07 +0300

    Using layout and yield content section

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,81 +1,26 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--Extend the Layout--}}
+@extends('layout')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-
-    <div class="container-fluid mb-5">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">My Simple Blog</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/posts">All Posts</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+{{--Declare the section --}}
+@section('content')
+    <div class="row mb-2">
+        <div class="col align-self-center">
+            <div class="card mb-3">
+                <h1 class="card-title">{{ $post->title }}</h1>
+                {!! $post->body !!}
+                <p class="card-text">
+                    <small class="text-muted">Last updated {{ $post->date }}</small>
+                </p>
             </div>
-        </nav>
-    </div>
-
-    <section class="blog-post">
-        <div class="container mt-20">
-            <div class="row mb-2">
-                <div class="col align-self-center">
-                    <div class="card mb-3">
-                        <h1 class="card-title">{{ $post->title }}</h1>
-                        {!! $post->body !!}
-                        <p class="card-text">
-                            <small class="text-muted">Last updated {{ $post->date }}</small>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-12">
-                    <a href="/" class="">Go Back Home</a>
-                </div>
-            </div>
-
         </div>
-    </section>
+    </div>
+    <div class="row mt-5">
+        <div class="col-12">
+            <a href="/" class="">Go Back Home</a>
+        </div>
+    </div>
+@endsection
 
 
 
