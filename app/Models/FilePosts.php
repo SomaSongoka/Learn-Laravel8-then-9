e5ac2d7955c4d1c6dd5eddb728777c0f2bc7b359
Josh L. Minga <joshlminga@gmail.com>
2022-02-22 17:52:27 +0300

    cache for 1 minute

--- app/Models/FilePosts.php
+++ app/Models/FilePosts.php
@@ -28,7 +28,7 @@ class FilePosts
     public static function all()
     {
         // To avoid re-reading the files every time: we are going to cache the posts files
-        return cache()->rememberForever('posts.all', function () {
+        return cache()->remember(now()->addMinutes(1),'posts.all', function () {
             /**
              * Reading files and content
              */
