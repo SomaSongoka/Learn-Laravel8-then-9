9ff8d62e1acb7024c238feb8064e27449e584ca7
Josh L. Minga <joshlminga@gmail.com>
2022-02-27 18:20:58 +0300

    Fillable and Protected

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * Fillable
+     *
+     * This is the array of fillable columns that can be mass assignable.
+     * In this list we have excluded id and created_at and updated_at columns.
+     * Means those can not be mass assigned.
+     *
+     *
+     */
+    protected $fillable = [
+        'title',
+        'excerpt',
+        'body',
+    ];
+
+    // Guarded is the opposite of fillable. = Means everything is fillable expect these
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    //But another way is to set the guarded to empty array  protected $guarded = []; and never sent user submitted form as array to Method create()
 }
