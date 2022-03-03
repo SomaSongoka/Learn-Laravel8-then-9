b34b9c88639da5a641d3f0b2954650aad7579ab5
Josh L. Minga <joshlminga@gmail.com>
2022-03-03 23:12:07 +0300

    Seed Table using factory

--- database/migrations/2022_02_27_100305_create_posts_table.php
+++ database/migrations/2022_02_27_100305_create_posts_table.php
@@ -15,14 +15,21 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->foreignId('category_id');
+            $table->foreignId('user_id'); // Laravel will assume you have users table with id column
+            //Another way to define foreign key (1:) we use unsignedBigInteger because our ID is AI
+            $table->unsignedBigInteger('category_id');
+
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
             $table->timestamp('published_at')->nullable();
+        });
+
+        // Then you can assign foreign key to the column
+        Schema::table('posts', function($table) {
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Now since we have Factory for User, Category and Post, we can seed the database
+
+        /** Incase you want to randomly crete all inputs except name for Model user then we can simply do */
+        $user = User::factory()->create([
+            'name' => 'Alpha Guru',
+        ]);
+        // with the help of Factory.
+        // We will not need to TRAUNCATE the tables, unless we are not running migrate:refresh first
+
+        Post::factory(5)->create([
+            //Incase we want to override user id with the user we created above
+            'user_id' => $user->id,
+        ]); // Create 5 posts
+        // NB: Post will create User and Category automatically
+
+        /*
         // Trancate the Table before seeding
+        Post::truncate();
         User::truncate();
+        Category::trauncate();
+
 
         // Seed the table with the data 1 user
         $user = User::factory()->create();
@@ -99,5 +118,6 @@ class DatabaseSeeder extends Seeder
             'excerpt' => '<p>Sixth Ipsum is simply post dummy text of the printing and typesetting industry.</p>',
             'body' => "<p>Sixth Ipsum simple blog post dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>",
         ]);
+        */
     }
 }
