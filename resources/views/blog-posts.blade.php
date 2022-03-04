0ae324caccb0101732f370b96c15b2a9467a385c
Josh L. Minga <joshlminga@gmail.com>
2022-03-04 22:42:39 +0300

    Loading featured post and passing variable to component

---
+++ resources/views/blog-posts.blade.php
@@ -0,0 +1,55 @@
+{{-- Extend the Layout --}}
+{{--@extends('components.layout')--}}
+
+{{-- Declare the section --}}
+{{--@section('content')--}}
+
+{{--    --}}{{-- Or we can just add values inside this area and it will be assign to variable slot inside our component --}}
+{{--    @foreach ($posts as $post)--}}
+{{--        <div class="col-md-4 col-sm-12 mb-5 {{ $loop->even ? 'foobar' : 'foo' }}">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-body">--}}
+{{--                    <a href="/blog/{{ $post->slug }}">--}}
+{{--                        <h1 class="card-title">{{ $post->title }} </h1>--}}
+{{--                    </a>--}}
+
+{{--                    <h6 class="card-title">--}}
+{{--                        Category: <a href="/blog/category/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--                    </h6>--}}
+
+
+{{--                    {!! $post->excerpt !!}--}}
+{{--                    <p class="card-text">--}}
+{{--                        <small class="text-muted">Last updated {{ $post->date }}</small>--}}
+{{--                    </p>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endforeach--}}
+{{--@endsection--}}
+
+<x-layout>
+
+{{-- We can load a partial section eg header here and name the file using underscore but that is not a must --}}
+    @include('_posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    {{-- Now we can load our post-featured-card component --}}
+    <x-post-featured-card :post="$posts[0]" />
+
+    <div class="lg:grid lg:grid-cols-2">
+        {{-- load our post-card component twice --}}
+        <x-post-card />
+        <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+        {{-- load our post-card component 3 times --}}
+        <x-post-card />
+        <x-post-card />
+        <x-post-card />
+        {{-- Now we can load our post-card component --}}
+    </div>
+</main>
+
+</x-layout>
