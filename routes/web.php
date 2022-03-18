15d11b744ebb4833ba56a9998ead7d70b37e29ee
Josh L. Minga <joshlminga@gmail.com>
2022-03-18 14:08:57 +0300

    Messy Search

--- routes/web.php
+++ routes/web.php
@@ -155,8 +155,22 @@ Route::get('/author/{user:username}', function (User $user) {
 
 // View All Posts
 Route::get('/blog', function () {
+
+    //Search The Messy Way
+    //1: Instead of Post::latest()->with('author')->get() {Get = execute the bilded query} we can do this
+    $posts = Post::latest()->with('author');
+    //2: Then let's check if request()->has('search') or you can write if(request('search')) | search is our GET request [name=search]
+    if (request()->has('search')) {
+        //3: If we have search then we will use the where() method to search the title
+
+        //NB: This will be executed only if the request has search. Here we now join the query building by adding the where clause
+        $posts->where('title', 'like', '%' . request()->get('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%'); // Check on Title and Body
+        // The above code can be $posts->where('title', 'like', '%' . request('search') . '%');
+    }
+
     return view('blog-posts', [
-        'posts' => Post::latest()->with('author')->get(),
+        'posts' => $posts->get(), //Post::latest()->with('author')->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
